3b7271a92649be6e1dac2b1ac64e35a8025fd156
Leonardo Rebouças <leoabcblm@gmail.com>
2022-09-23 00:14:54 -0300

    Componente Card

--- redux-simples/src/App.jsx
+++ redux-simples/src/App.jsx
@@ -8,12 +8,18 @@ function App() {
     <div className="App">
       <h1>Exercício React - Redux (Simples)</h1>
       <div className="linha">
-        <Card title="Card 1">
+        <Card title="Card 1" red>
+          Conteúdo
+        </Card>
+        <Card title="Card 2" green>
           Conteúdo
         </Card>
       </div>
       <div className="linha">
-        <Card title="Card 2">
+        <Card title="Card 3" blue>
+          Y
+        </Card>
+        <Card title="Card 4" purple>
           Y
         </Card>
       </div>
